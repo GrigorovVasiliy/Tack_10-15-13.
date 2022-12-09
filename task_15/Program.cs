a469eab1cf13691ec9d3bd67b862c9f4ad8e78a4
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-09 23:50:53 +0700

    15

---
+++ task_15/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите день недели: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+while(a < 1 || a > 7){
+    Console.WriteLine("Вы ошиблись, введите повторно: ");
+    a = Convert.ToInt32(Console.ReadLine());}
+if (a == 6 || a == 7)
+    Console.WriteLine("Это выходной!");
+else
+    Console.WriteLine("Будний день");
