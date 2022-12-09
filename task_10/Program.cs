572a4c5813aa58bc3cba1202d332af0133b8b389
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-09 22:47:55 +0700

    10

---
+++ task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Веедите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine()), n1, n2, n3;
+
+n1 = n / 100;
+n2 = n / 10 % 10;
+n3 = n % 10;
+
+Console.WriteLine($"Вторая цифра числа: {n2}.");
