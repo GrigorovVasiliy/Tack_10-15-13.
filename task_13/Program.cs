a3282562f774f653f635c306283876b764d0ca80
Grigorov_Vasiliy <Grigorov@internet.ru>
2022-12-09 23:43:31 +0700

    13

---
+++ task_13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+
+Console.Write("Веедите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 100)
+    Console.WriteLine($"Число имеет меньше 3-х цифр");
+else
+{
+    while (n > 999) 
+    n = n / 10;
+    Console.WriteLine($"Третья цифра числа: { n % 10}.");
+}
+
